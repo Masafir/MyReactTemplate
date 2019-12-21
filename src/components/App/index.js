4ee651d21f8ad7fe0b0be9372bc2abe238b612b3
Masafir <lachamir@outlook.fr>
2019-12-21 22:04:35 +0100

    initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,24 @@
+/**
+ * NPM import
+ */
+import React from 'react';
+
+/**
+ * Local import
+ */
+import './app.scss';
+
+/**
+ * Code
+ */
+const App = () => (
+  <div>
+
+    React App
+  </div>
+);
+
+/**
+ * Export
+ */
+export default App;
